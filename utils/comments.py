6bc26ad813c226be2db7f058e9ef1a2082286b1a
onlyphantom <samuel@outlook.sg>
2022-05-11 21:16:39 +0700

    add process_comments, make_csv helper functions

---
+++ utils/comments.py
@@ -0,0 +1,43 @@
+import csv
+from datetime import datetime as dt
+
+comments = []
+today = dt.today().strftime('%d-%m-%Y')
+
+def process_comments(response_items, csv_output=False):
+
+    for res in response_items:
+
+        # loop through the replies
+        if 'replies' in res.keys():
+            for reply in res['replies']['comments']:
+                comment = reply['snippet']
+                comment['commentId'] = reply['id']
+                comments.append(comment)
+        else:
+            comment = {}
+            comment['snippet'] = res['snippet']['topLevelComment']['snippet']
+            comment['snippet']['parentId'] = None
+            comment['snippet']['commentId'] = res['snippet']['topLevelComment']['id']
+
+            comments.append(comment['snippet'])
+
+    if csv_output:
+         make_csv(comments)
+    
+    print(f'Finished processing {len(comments)} comments.')
+    return comments
+
+
+def make_csv(comments, channelID=None):
+    header = comments[0].keys()
+
+    if channelID:
+        filename = f'comments_{channelID}_{today}.csv'
+    else:
+        filename = f'comments_{today}.csv'
+
+    with open(filename, 'w', encoding='utf8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(comments)
--- yt_public.py
+++ yt_public.py
@@ -1,6 +1,10 @@
 import os
+import csv
+from datetime import datetime as dt
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
+
+from utils.comments import process_comments, make_csv
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -9,7 +13,7 @@ youtube = build("youtube", "v3", developerKey=API_KEY)
 
 def search_result(query):
     """
-    Refer: https://googleapis.github.io/google-api-python-client/docs/dyn/youtube_v3.search.html
+    Refer to the documentation: https://googleapis.github.io/google-api-python-client/docs/dyn/youtube_v3.search.html
     """
     request = youtube.search().list(
         part="snippet",
@@ -29,12 +33,33 @@ def channel_stats(channelID):
     )
     return request.execute()
 
-def comment_threads(channelID):
+def comment_threads(channelID, to_csv=False):
+    
+    comments_list = []
+    
     request = youtube.commentThreads().list(
         part='id,replies,snippet',
         videoId=channelID,
     )
-    return request.execute()
+    response = request.execute()
+    comments_list.extend(process_comments(response['items']))
+
+    # if there is nextPageToken, then keep calling the API
+    while response.get('nextPageToken', None):
+        request = youtube.commentThreads().list(
+            part='id,replies,snippet',
+            videoId=channelID,
+            pageToken=response['nextPageToken']
+        )
+        response = request.execute()
+        comments_list.extend(process_comments(response['items']))
+    
+    print(f"Finished fetching comments for {channelID}. {len(comments_list)} comments found.")
+    
+    if to_csv:
+        make_csv(comments_list, channelID)
+    
+    return comments_list
 
 if __name__ == '__main__':
     pyscriptVidId = 'Qo8dXyKXyME'
@@ -42,6 +67,6 @@ if __name__ == '__main__':
 
     # response = search_result("pyscript")
     # response = channel_stats(channelId) 
-    response = comment_threads(pyscriptVidId)
+    response = comment_threads(pyscriptVidId, to_csv=True)
 
-    print(response)
+    # print(response)
