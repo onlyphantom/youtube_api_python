e8d3c8b5d2d796ea9cd36bebabf1afacd0bdb8a2
onlyphantom <samuel@outlook.sg>
2023-05-01 20:47:38 +0700

    add export to csv helper function in yt_auth

--- yt_auth.py
+++ yt_auth.py
@@ -1,7 +1,6 @@
-import os
-from venv import create
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
+import csv
 
 from tabulate import tabulate
 
@@ -9,11 +8,13 @@ SCOPES = ['https://www.googleapis.com/auth/yt-analytics.readonly']
 
 API_SERVICE_NAME = 'youtubeAnalytics'
 API_VERSION = 'v2'
-CLIENT_SECRETS_FILE = 'client_secret_desktop_temp.json'
+CLIENT_SECRETS_FILE = 'client_secret.json'
 
 def get_service():
   flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-  credentials = flow.run_local_server()
+  credentials = flow.run_console()
+  # alternatively (https://github.com/onlyphantom/youtube_api_python/pull/3/files):
+  # credentials = flow.run_local_server()
   return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
 
 def execute_api_request(client_library_function, **kwargs):
@@ -32,6 +33,12 @@ def create_table(table, headers=None):
     for row in table:
         print(rowstring.format(*row))
 
+def create_csv(table, headers=None, filename='output.csv'):
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        if headers:
+            writer.writerow(headers)
+        writer.writerows(table)
 
 if __name__ == '__main__':
 
@@ -39,14 +46,14 @@ if __name__ == '__main__':
     result = execute_api_request(
         youtubeAnalytics.reports().query,
         ids='channel==MINE',
-        startDate='2022-01-01',
-        endDate='2022-04-30',
+        startDate='2022-05-01',
+        endDate='2023-04-30',
         metrics='estimatedMinutesWatched,views,likes,subscribersGained',
         dimensions='day',
         sort='day'
     )
     headers = ['date', 'estMinutesWatched', 'views', 'likes', 'subscribersGained']
     # create_table(result['rows'], headers=headers)
+    create_csv(result['rows'], headers=headers)
     print(tabulate(result['rows'], headers=headers, tablefmt="pretty"))
-    
-    # print(result)
+  
